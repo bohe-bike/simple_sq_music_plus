735b1325d07267d8d4935fe917238480dd1941e8
coco-bike <444503829@qq.com>
2026-04-16 14:41:18 +0800

    报错修改

--- src/main/java/com/sqmusicplus/v3/plug/netease/hander/NeteaseHander.java
+++ src/main/java/com/sqmusicplus/v3/plug/netease/hander/NeteaseHander.java
@@ -22,6 +22,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
